fbcf8b1078758cad8151edf983ef29d88fa12a2d
Vadim-Nazarov <107613708+Vadim-Nazarov@users.noreply.github.com>
2024-08-29 10:03:09 +0300

    Update main.tf

--- clopro/manifests/main.tf
+++ clopro/manifests/main.tf
@@ -1,1 +1,16 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+  required_version = ">=0.13"
+}
+
+provider "yandex" {
+   token = var.yc_token
+   cloud_id  = "b1ga397ao4qr22ofjshl"
+   folder_id = "b1golull73609mhvvdf6"
+   zone      = "ru-central1-a"
+  }
 
