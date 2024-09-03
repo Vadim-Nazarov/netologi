e0c585626308d8260b2cd3920bb2697bf8c49d12
Vadim-Nazarov <107613708+Vadim-Nazarov@users.noreply.github.com>
2024-09-03 13:52:30 +0300

    Create vars.tf

---
+++ clopro-2/terraform files/vars.tf
@@ -0,0 +1,11 @@
+variable "yc_token" {
+   default = "PI2S8AABMd2fgsN5MRI6C5WYI6FEH0_HXvQ"
+}
+
+variable "sa-access-key" {
+   default = "BRgWTTa-"
+}
+
+variable "sa-secret-key" {
+   default = "YCMzf_jHV_tHQ7CLgl"
+}
